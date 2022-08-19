90fb5a6b2c0397eb9ee74a1eba691b892c4aa0a5
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-08-19 11:53:41 -0300

    adicionando a função para pesquisar com a tecla Enter

--- src/Components/SearchBar/index.jsx
+++ src/Components/SearchBar/index.jsx
@@ -1,36 +1,36 @@
-import Style from "./style"
-import Button from "../Button"
-import {useState} from "react"
-import useGithub from "../../hooks/github-hooks"
+import Style from "./style";
+import Button from "../Button";
+import { useState } from "react";
+import useGithub from "../../hooks/github-hooks";
 
+function SearchBar() {
+  const [userName, setUserName] = useState("");
+  const { getUser } = useGithub();
 
-function SearchBar(){
-    const [userName, setUserName] = useState("")
-    const {getUser} = useGithub()
+  function handleOnChange(e) {
+    setUserName(e.target.value);
+  }
 
-    function handleOnChange(e){
-        setUserName(e.target.value)
-    }
+  function handleSubmit(e) {
+    getUser(userName);
+  }
 
-    function handleSubmit(e){
-        getUser(userName)
+  return (
+    <Style.Wrapper>
+      <Style.IconSearch src="images/glass_icon.svg" />
+      <Style.Input
+        type="text"
+        placeholder="Digite um usuario para Pesquisar"
+        onChange={handleOnChange}
+        value={userName}
+        onKeyDown={(e) => e.key == "Enter" && handleSubmit()}
+      />
 
-    }
-
-    return (
-        <Style.Wrapper >
-            <Style.IconSearch src="images/glass_icon.svg" />
-            <Style.Input 
-                type="text" placeholder="Digite um usuario para Pesquisar"
-                onChange={handleOnChange}
-                value={userName}
-             />
-
-            <Style.WrapperButton>
-                <Button text="Search" onClick={handleSubmit}/>
-            </Style.WrapperButton>
-        </Style.Wrapper>
-    )
+      <Style.WrapperButton>
+        <Button text="Search" onClick={handleSubmit} />
+      </Style.WrapperButton>
+    </Style.Wrapper>
+  );
 }
 
-export default SearchBar
+export default SearchBar;
