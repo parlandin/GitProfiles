e8bbc50a90187b5fdf973005b18bd2d001a8bfc7
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-08-19 13:50:20 -0300

    adicioando espaçamento global

--- src/Components/layout/style.js
+++ src/Components/layout/style.js
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
-const Style = {}
+const Style = {};
 
-Style.Wrapper = styled.div `
-    background-color: #141C2F;
-    width:100%;
-    min-height:100vh;
-    height:100%;
-`
+Style.Wrapper = styled.div`
+  background-color: #141c2f;
+  width: 100%;
+  min-height: 100vh;
+  height: 100%;
+  padding: 5px;
+`;
 
 export default Style;
